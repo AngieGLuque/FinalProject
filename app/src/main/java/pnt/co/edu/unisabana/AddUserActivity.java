312441a2902fa8c0e27ea9a5dc90ba181a986f35
IlI <mariovaac@unisabana.edu.co>
2019-05-26 16:51:13 -0500

    Mario

--- app/src/main/java/pnt/co/edu/unisabana/AddUserActivity.java
+++ app/src/main/java/pnt/co/edu/unisabana/AddUserActivity.java
@@ -12,6 +12,11 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddUserActivity extends AppCompatActivity {
     Switch carreraDependencia;
@@ -34,6 +39,7 @@ public class AddUserActivity extends AppCompatActivity {
                 startActivity(open);
             }
         });
+
     }
     private void check(){
         if(carreraDependencia.isChecked()){
--- app/src/main/java/pnt/co/edu/unisabana/GitHubRepo.java
+++
@@ -1,9 +0,0 @@
-package pnt.co.edu.unisabana;
-
-public class GitHubRepo {
-    private String name;
-
-    public String getName(){
-        return name;
-    }
-}
