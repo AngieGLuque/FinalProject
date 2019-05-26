40a55fb7eaff3a226e71f424972847f72aa78995
AngieGLuque <angie.luque08@gmail.com>
2019-05-26 14:10:36 -0500

    fg

--- app/src/main/java/pnt/co/edu/unisabana/MainActivity.java
+++ app/src/main/java/pnt/co/edu/unisabana/MainActivity.java
@@ -1,7 +1,10 @@
 package pnt.co.edu.unisabana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton user_Create = findViewById(R.id.newUser);
+        ImageButton user_Login = findViewById(R.id.login);
+        user_Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open = new Intent(MainActivity.this,AddUserActivity.class);
+                startActivity(open);
+            }
+        });
+        user_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(open);
+            }
+        });
     }
 }
