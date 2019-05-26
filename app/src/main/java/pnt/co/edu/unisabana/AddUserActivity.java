48a2271f2bd5bdd2a241d0bac3240955fda71a4e
AngieGLuque <angie.luque08@gmail.com>
2019-05-26 16:48:38 -0500

    fg

--- app/src/main/java/pnt/co/edu/unisabana/AddUserActivity.java
+++ app/src/main/java/pnt/co/edu/unisabana/AddUserActivity.java
@@ -12,11 +12,6 @@ import android.widget.Toast;
 
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddUserActivity extends AppCompatActivity {
     Switch carreraDependencia;
@@ -39,9 +34,6 @@ public class AddUserActivity extends AppCompatActivity {
                 startActivity(open);
             }
         });
-
-        GitHubClient client = ServiceGenerator.createService();
-
     }
     private void check(){
         if(carreraDependencia.isChecked()){
