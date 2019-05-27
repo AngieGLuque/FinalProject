37de577df11428ac22d071ad156a7f0c7838f71c
IlI <mariovaac@unisabana.edu.co>
2019-05-26 19:24:06 -0500

    Mario

---
+++ app/src/main/java/pnt/co/edu/unisabana/HerokuService.java
@@ -0,0 +1,15 @@
+package pnt.co.edu.unisabana;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface HerokuService {
+
+
+
+}
--- app/src/main/java/pnt/co/edu/unisabana/ShowDataActivity.java
+++ app/src/main/java/pnt/co/edu/unisabana/ShowDataActivity.java
@@ -1,7 +1,10 @@
 package pnt.co.edu.unisabana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ShowDataActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ShowDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_data);
+        ImageButton cancel = findViewById(R.id.redx);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open = new Intent(ShowDataActivity.this,MainActivity.class);
+                startActivity(open);
+            }
+        });
     }
 }
